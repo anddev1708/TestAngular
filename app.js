cef740cd6e6fd079232b88651d45ddb0d940b04e
quyetqv <quyetqv@gmail.com>
2017-10-24 06:43:34 +0700

    Test Angular

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home',
+  'myApp.register'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
+
+
+// 'myApp.register',
+// 'myApp.welcome',
+// 'myApp.addPost',
+// 'myApp.test'
